76898a614f5fc450bd2e134e5647853bd3b8f2b3
drag13 <vitaliy.ruban@itera.no>
2022-05-04 15:42:05 +0300

    Initial commit

---
+++ src/context/contacts.tsx
@@ -0,0 +1,5 @@
+export const Contacts = () => {
+  //const { twitter } = useContext(AuthorDataContext);
+  const twitter = "??? How to get my twitter???";
+  return <p>Feel free to contact me in twitter: {twitter}</p>;
+};
