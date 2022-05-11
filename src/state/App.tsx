41c081de34f2731262a834222e9383d505869a0e
drag13 <test@test>
2022-05-11 06:09:33 -0700

    Added correct version of the state

--- src/state/App.tsx
+++ src/state/App.tsx
@@ -1,57 +1,42 @@
-import { makeAutoObservable, runInAction } from "mobx";
-import { observer } from "mobx-react-lite";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
-class EmployeeStore {
-  salary = 100;
-  onBench = true;
+type EmployeeData = {
+  salary: number;
+  onBench: boolean;
+};
 
-  constructor() {
-    makeAutoObservable(this);
-  }
+const data: EmployeeData = { salary: 100, onBench: true };
 
-  increaseSalary(v: number) {
-    this.salary = this.salary + v;
-  }
-}
-
-const employeeStore = new EmployeeStore();
-
-const Dashboard = () => (
+const Dashboard = ({ salary }: { salary: number }) => (
   <div>
     <h1>Dashboard</h1>
-    <EmployeeCard />
+    <EmployeeCard salary={salary} />
   </div>
 );
 
-const Project = observer(() => {
-  const { onBench } = employeeStore;
+const Project = ({ onBench }: { onBench: boolean }) => {
   useEffect(() => {
     // alert("UPDATED");
   });
   return <div>{onBench ? "OnBench" : "Project "}</div>;
-});
+};
 
-const EmployeeCard = observer(() => {
+const EmployeeCard = ({ salary }: { salary: number }) => {
   return (
     <div>
       <h2>About the employee</h2>
-      <div>Salary: {employeeStore.salary}</div>
-      <div>
-        <button onClick={() => employeeStore.increaseSalary(200)}>
-          Increase Salary
-        </button>
-      </div>
-      <Project />
+      <div>Salary: {salary}</div>
+      {/* <Project onBench={onBench} /> */}
     </div>
   );
-});
+};
 
 function App() {
+  const [employee] = useState(data);
   return (
     <div className="app">
-      <Dashboard />
+      <Dashboard salary={employee.salary} />
     </div>
   );
 }
